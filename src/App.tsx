9468ea55574e85f17ebce0e9bf8b431abdc983ce
rakibnsajib <rakibnsajib@gmail.com>
2026-01-06 07:21:04 +0600

    update 3

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
       <div className="min-h-screen bg-background">
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/version-2">
           <Routes>
             <Route path="/" element={<Bio />} />
             <Route path="/bio" element={<Navigate to="/" replace />} />
